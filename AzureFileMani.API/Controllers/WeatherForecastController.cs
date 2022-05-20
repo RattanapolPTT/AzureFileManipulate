edfc13d687e5f9682d354ebbdf2c40a02f236f1f
rattanapol.p <rattanapol.p@pttdigital.com>
2022-05-20 10:27:07 +0700

    Add testapi

--- AzureFileMani.API/Controllers/WeatherForecastController.cs
+++ AzureFileMani.API/Controllers/WeatherForecastController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-
+using System;
+using System.IO;
+using System.Collections.Generic;
 namespace AzureFileMani.API.Controllers;
 
 [ApiController]
@@ -18,15 +20,45 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    //[HttpGet(Name = "GetWeatherForecast")]
+    //public IEnumerable<WeatherForecast> Get()
+    //{
+    //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    //    {
+    //        Date = DateTime.Now.AddDays(index),
+    //        TemperatureC = Random.Shared.Next(-20, 55),
+    //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+    //    })
+    //    .ToArray();
+    //}
+
+    [HttpGet]
+    public List<FileInfo> TestApi()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        List<FileInfo> path = new List<FileInfo>();
+        string[] files = Directory.GetFiles(@"./fileBuffer", "*");
+        foreach (var file in files)
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            var fileInfo = new FileInfo();
+            fileInfo.Path = file;
+            path.Add(fileInfo);
+            //path.Add(file);
+        }
+
+        string[] subdirectoryEntries = Directory.GetDirectories(@"./fileBuffer");
+
+        foreach (string subdirectory in subdirectoryEntries)
+        {
+            var fileInfo = new FileInfo();
+            fileInfo.Path = subdirectory;
+            path.Add(fileInfo);
+        }
+        return path;
     }
+
+    public class FileInfo
+    {
+        public string Path { get; set; }
+    }
+
 }
